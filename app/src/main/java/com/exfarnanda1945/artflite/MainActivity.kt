3cd6b98a6595da319e01b6f8c0971087426c5f2d
miko <exfar@samir.co.id>
2024-08-07 13:48:19 +0700

    add camera granted state

--- app/src/main/java/com/exfarnanda1945/artflite/MainActivity.kt
+++ app/src/main/java/com/exfarnanda1945/artflite/MainActivity.kt
@@ -8,19 +8,25 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.content.ContextCompat
 import com.exfarnanda1945.artflite.ui.theme.ArtfliteTheme
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
 
 class MainActivity : ComponentActivity() {
+    private val _isCameraGranted = MutableStateFlow(false)
+    private val cameraIsGranted = _isCameraGranted.asStateFlow()
+
     private val cameraPermissionRequest =
         registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
+            _isCameraGranted.value = isGranted
+
             Toast.makeText(
                 this@MainActivity,
                 "Camera permission is ${if (isGranted) "Granted" else "Denied"}",
@@ -33,34 +39,24 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
 
         when (PackageManager.PERMISSION_GRANTED) {
-            ContextCompat.checkSelfPermission(this@MainActivity, Manifest.permission.CAMERA) -> {}
+            ContextCompat.checkSelfPermission(this@MainActivity, Manifest.permission.CAMERA) -> {
+                _isCameraGranted.value = true
+            }
+
             else -> cameraPermissionRequest.launch(Manifest.permission.CAMERA)
         }
         setContent {
             ArtfliteTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    Box(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(innerPadding), contentAlignment = Alignment.Center
+                    ) {
+                        CameraPreviewScreen(isCameraGranted = cameraIsGranted)
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ArtfliteTheme {
-        Greeting("Android")
     }
 }
