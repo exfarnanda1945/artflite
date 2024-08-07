214a0dfccec371791419b5d6c1859106d1035b21
miko <exfar@samir.co.id>
2024-08-07 10:39:15 +0700

    camera request permission

--- app/src/main/java/com/exfarnanda1945/artflite/MainActivity.kt
+++ app/src/main/java/com/exfarnanda1945/artflite/MainActivity.kt
@@ -1,9 +1,13 @@
 package com.exfarnanda1945.artflite
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -11,12 +15,27 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.content.ContextCompat
 import com.exfarnanda1945.artflite.ui.theme.ArtfliteTheme
 
 class MainActivity : ComponentActivity() {
+    private val cameraPermissionRequest =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
+            Toast.makeText(
+                this@MainActivity,
+                "Camera permission is ${if (isGranted) "Granted" else "Denied"}",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
+        when (PackageManager.PERMISSION_GRANTED) {
+            ContextCompat.checkSelfPermission(this@MainActivity, Manifest.permission.CAMERA) -> {}
+            else -> cameraPermissionRequest.launch(Manifest.permission.CAMERA)
+        }
         setContent {
             ArtfliteTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
