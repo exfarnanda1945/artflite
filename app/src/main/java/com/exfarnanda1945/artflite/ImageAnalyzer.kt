ad9c548a0c6f4c9b627fdd5cbd95557c649493ac
miko <exfarnanda1945@gmail.com>
2024-08-11 20:43:31 +0700

    add image analyzer implement image analysis

---
+++ app/src/main/java/com/exfarnanda1945/artflite/ImageAnalyzer.kt
@@ -0,0 +1,19 @@
+package com.exfarnanda1945.artflite
+
+import androidx.camera.core.ImageAnalysis
+import androidx.camera.core.ImageProxy
+
+class ImageAnalyzer(
+    private val detector: ObjectDetection,
+    private val onResult: (List<Classification>) -> Unit
+) : ImageAnalysis.Analyzer {
+    override fun analyze(image: ImageProxy) {
+        val rotationDegree = image.imageInfo.rotationDegrees
+        val bitmap = image.toBitmap()
+
+        val result = detector.detect(bitmap, rotationDegree)
+        onResult(result)
+
+        image.close()
+    }
+}
